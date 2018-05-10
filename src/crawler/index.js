dae54db7fd455df68720288ffd20bb8f813190b2
Husnul Anwari <husnulhamidiah@gmail.com>
2018-05-11 06:16:31 +0700

    Update media crawler

--- src/crawler/index.js
+++ src/crawler/index.js
@@ -6,52 +6,62 @@ import Promise from 'bluebird'
 import ora from 'ora'
 import { V1 as Client } from 'instagram-private-api'
 
-dotenv.load()
+dotenv.config()
 
 Promise.promisifyAll(redis)
-const client = redis.createClient({ db: 2 })
+const redisClient = redis.createClient({ db: 2 })
 
 const { BOT_IG_USERNAME, BOT_IG_PASSWORD, BOT_TARGET_USERNAME,
-  BOT_TARGET_LIMIT, BOT_TARGET_INDEX, BOT_MEDIA_LIMIT, BOT_DEVICE_PREFIX } = process.env
-const COOKIE_FILE_PATH = path.resolve(__dirname, `../../cookies/${BOT_IG_USERNAME}.json`)
-const REDIS_LIST_KEY = `_LIST__${BOT_IG_USERNAME}_${BOT_TARGET_USERNAME}`
+  BOT_MEDIA_LIMIT, BOT_LIKER_LIMIT, BOT_TARGET_INDEX, REDIS_KEY_PREFIX } = process.env
 
-const device = new Client.Device(`${BOT_DEVICE_PREFIX}_${BOT_IG_USERNAME}`)
-const storage = new Client.CookieFileStorage(COOKIE_FILE_PATH)
+const device = new Client.Device(`GOOGLE_PIXEL_PANDA_${BOT_IG_USERNAME}`)
+const storage = new Client.CookieFileStorage(path.resolve(__dirname, `../../cookies/${BOT_IG_USERNAME}.json`))
 
-const getUserMedia = async (session, id, limit = false) => {
-  spinner.start(`Fetching magic ingredient #${id}...`)
+const getUserMedia = (session, id, limit = false) => {
+  spinner.start(`Fetching ${id} media`)
   let feed = new Client.Feed.UserMedia(session, id, limit)
   feed.map = medium => medium.params
-  return feed.all()
-    .catch((error) => error && Promise.resolve(undefined))
+  return feed.all().catch((error) => error && Promise.resolve(undefined))
 }
 
-const getLikers = (session, id) => Client.Media.likers(session, id)
-  .then((likers) => Promise.resolve(likers.map((liker) => liker.params)))
+const getLikers = async (session, id) => {
+  try {
+    const likers = await Client.Media.likers(session, id)
+    return Promise.resolve(likers.map(liker => liker.params))
+  } catch (error) {
+    return Promise.resolve(undefined)
+  }
+}
 
-const getLikersMedia = (session, media, index = 1) =>
-  Promise.map(media, (medium) => getLikers(session, medium.id), { concurrency: 5 })
-    .then((likers) => Promise.resolve(_.uniq(_.flatten(likers))))
-    .then((likers) => Promise.map(_.slice(likers, 0, BOT_MEDIA_LIMIT), (liker) =>
-      getUserMedia(session, liker.id, 1), { concurrency: 5 }))
-    .then((media) => Promise.resolve(_.compact(media).map(medium =>
-      (_.nth(medium, index)) ? _.nth(medium, index).id : undefined)))
-    .catch((error) => console.log(error) && Promise.reject(error))
+const getLikersMedia = async (session, media, index = 1) => {
+  try {
+    const likers = await Promise.map(media, (medium) => getLikers(session, medium.id), { concurrency: 5 })
+    const userMedia = await Promise.map(_.compact(_.slice(_.uniq(_.flatten(likers)), 0, BOT_LIKER_LIMIT)), (liker) => getUserMedia(session, liker.id, 1), { concurrency: 5 })
+    return Promise.resolve(_.compact(userMedia).map(medium => (_.nth(medium, index)) ? _.nth(medium, index).id : undefined))
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
 
-const storeMediaToRedis = (redisClient, key, media) =>
-  redisClient.rpushAsync.apply(redisClient, [key].concat(_.compact(media)))
+const storeMediaToRedis = (client, key, media) => {
+  spinner.start(`Saving media to redis`)
+  return client.rpushAsync.apply(client, [key].concat(_.compact(media)))
+}
 
-const spinner = ora('Starting magic generator machine...').start()
-Client.Session.create(device, storage, BOT_IG_USERNAME, BOT_IG_PASSWORD)
-  .then((session) => {
-    return Client.Account.searchForUser(session, BOT_TARGET_USERNAME)
-      .then((account) => getUserMedia(session, account.params.id, BOT_TARGET_LIMIT))
-      .then((media) => getLikersMedia(session, media, BOT_TARGET_INDEX))
-      .tap((media) => storeMediaToRedis(client, REDIS_LIST_KEY, media))
-      .then((media) => {
-        spinner.succeed(`${media.length} magics created at ${REDIS_LIST_KEY}...`)
-        return process.exit(0)
-      })
-  })
-  .catch((error) => (console.error(error) && process.exit(1)))
+const spinner = ora('Starting crawler bot').start();
+
+(async () => {
+  try {
+    const session = await Client.Session.create(device, storage, BOT_IG_USERNAME, BOT_IG_PASSWORD)
+    const account = await Client.Account.searchForUser(session, BOT_TARGET_USERNAME)
+    const userMedia = await getUserMedia(session, account.params.id, BOT_MEDIA_LIMIT)
+    const likersMedia = await getLikersMedia(session, userMedia, BOT_TARGET_INDEX)
+    await storeMediaToRedis(redisClient, REDIS_KEY_PREFIX + BOT_IG_USERNAME, likersMedia)
+
+    spinner.succeed(`Successfully save ${likersMedia.length} media`)
+    return process.exit(0)
+  } catch (error) {
+    console.log(error)
+    return process.exit(1)
+  }
+})()
